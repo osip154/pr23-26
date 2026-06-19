fb29821e1b245b8f23c70041f6895338aa7e3955
osip154 <ilyaosip1208@gmail.com>
2026-06-19 21:14:19 +0700

    готово

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pr23"
+    namespace = "com.example.pr23-26"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.pr23"
+        applicationId = "com.example.pr23-26"
         minSdk = 31
         targetSdk = 36
         versionCode = 1
